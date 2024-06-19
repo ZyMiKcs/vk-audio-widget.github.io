d39faf69329424fd14ab3b62c173a6156afe146a
ZyMiK <zymik.official@gmail.com>
2024-06-19 18:31:06 +0300

    Some problems

--- src/utils/pauseAnimation.js
+++
@@ -1,6 +0,0 @@
-export const pauseAnimation = () => {
-    const bars = document.querySelectorAll('.bar');
-    bars.forEach((bar) => {
-        bar.style.animationPlayState = 'paused';
-    });
-};
